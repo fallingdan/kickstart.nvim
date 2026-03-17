6b8ee364a102862e4b30e5333257d2fcb99a628e
Dan <onewaylimited@gmail.com>
2026-03-17 07:44:11 -0500

    Adds python support

--- init.lua
+++ init.lua
@@ -502,7 +502,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -593,6 +593,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        python = { 'ruff' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
